c9f0da81a44fb51986b53c0972c31a0c6480898d
mitriifd <idimka9@gmail.com>
2022-01-26 21:06:45 +0500

    добавление наложения текста

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 initPlayer({
-    target: '.my-player',
-    slides: [
-        { url: 'img/1.jpg', alt: 'img1' },
-        { url: 'img/2.jpg', alt: 'img2' },
-        { url: 'img/3.jpg', alt: 'img3' },
-        { url: 'img/4.jpg', alt: 'img4' },
-    ],
-    delayPerSlide: 5
-})
+	target: '.my-player',
+	slides: [
+		{
+			url: 'img/1.jpg',
+			alt: 'img1',
+			overlays: [ 
+                { 
+                    type: 'text', 
+                    value: 'Привет',
+                    styles: {
+                        color: 'orange',
+                        'font-size': '60px',
+                        'text-shadow': '1px 1px #000',
+                        top: '60%',
+                        left: '30%',
+                        'transform': 'rotate(-30deg)',
+                        animation: 'scale 2s infinite ease-in-out',
+                    }
+                },
+                {
+                    type: 'text',
+                    value: 'Привет мир',
+                    styles: {
+                        color: 'orange',
+                        'font-size': '20px',
+                        'text-shadow': '1px 1px #000',
+                        bottom: '10%',
+                        right: '30%',
+                        'transform': 'rotate(90deg)',
+                        animation: 'scale 6s infinite ease-in-out',
+                    }
+                },
+            ],
+		},
+		{ url: 'img/2.jpg', alt: 'img2' },
+		{ url: 'img/3.jpg', alt: 'img3' },
+		{ url: 'img/4.jpg', alt: 'img4' },
+	],
+	delayPerSlide: 5,
+});
--- player/lib.js
+++ player/lib.js
@@ -68,8 +68,35 @@ function initPlayer(params) {
         return `
             <div class="player-chunk ${isFirst ? 'player-chunk--active' : ''}">
                 <img class="player-img" src="${slide.url}" alt="${slide.alt || ''}">
+                ${generateOverlays(slide)}
             </div>
         `;
+    }
+    function generateOverlays(slide) {
+        if (slide.overlays === undefined) {
+            return ''
+        }
+
+        let res = '';
+
+        for (const el of slide.overlays) {
+            const styles = (el.styles !== undefined ? Object.entries(el.styles) : [])
+                .map((el) => el.join(':'))
+                .join(';')
+            res += `<div class="player-chunck-overlay" style="${styles}">${renderOverlay(el)}</div>`;
+        }
+
+        return res;
+
+        function renderOverlay(overlay) {
+            if (overlay.type === 'text') {
+                return overlay.value;
+            }
+            if (overlay.type === 'img') {
+                return `<img src="${overlay.value}" alt="" />`
+            }
+            return '';
+        }
     }
     function generatePlayerLayout() {
         return `
